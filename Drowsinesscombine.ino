59e11b484edd67550c17f1f9752e4186de564811
PMorey22 <110521793+PMorey22@users.noreply.github.com>
2023-08-01 20:09:20 +0530

    Add files via upload

---
+++ Drowsinesscombine.ino
@@ -0,0 +1,205 @@
+/*
+This code should work to get warning cross the buzzer when something be closer than 0.5 meter
+Circuit is ultrasonic sensor and buzzer +5v and Arduino uno is used
+a_atef45@yahoo.com
+www.zerosnones.net
++201153300223
+*/
+// Define pins for ultrasonic and buzzer
+int const trigPin = 10;
+int const echoPin = 9;
+int const buzzPin = 13  ;
+//int BUZZER = 10;   
+int ALCOHOL_sensor = 4;// MQ-3 SENSOR  
+int ALCOHOL_detected; 
+
+
+#include <Adafruit_Fingerprint.h>
+
+
+#if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
+// For UNO and others without hardware serial, we must use software serial...
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+// Set up the serial port to use softwareserial..
+SoftwareSerial mySerial(2, 3);
+
+#else
+// On Leonardo/M0/etc, others with hardware serial, use hardware serial!
+// #0 is green wire, #1 is white
+#define mySerial Serial1
+
+#endif
+
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+
+
+void setup()
+{
+pinMode(trigPin, OUTPUT); // trig pin will have pulses output
+pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
+pinMode(buzzPin, OUTPUT); // buzz pin is output to control buzzering
+Serial.begin(9600);  
+//pinMode(BUZZER, OUTPUT);  
+pinMode(ALCOHOL_sensor, INPUT);
+
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nAdafruit finger detect test");
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  delay(5);
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+     }
+      getFingerprintID();
+  delay(50);
+
+  Serial.println(F("Reading sensor parameters"));
+  finger.getParameters();
+  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
+  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
+  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
+  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
+  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
+  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
+  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
+
+  finger.getTemplateCount();
+
+  if (finger.templateCount == 0) {
+    Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
+  }
+  else {
+    Serial.println("Waiting for valid finger...");
+      Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+      
+  }
+
+}
+void loop()
+{
+// Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
+int duration, distance;
+// Output pulse with 1ms width on trigPin
+digitalWrite(trigPin, HIGH);
+delay(1);
+digitalWrite(trigPin, LOW);
+// Measure the pulse input in echo pin
+duration = pulseIn(echoPin, HIGH);
+// Distance is half the duration devided by 29.1 (from datasheet)
+distance = (duration/2) / 29.1;
+// if distance less than 0.5 meter and more than 0 (0 or less means over range)
+if (distance <= 30 && distance >= 0) {
+// Buzz
+tone(buzzPin,1000); 
+digitalWrite(buzzPin, HIGH);
+} else {
+// Don't buzz
+noTone(buzzPin);
+}
+// Waiting 60 ms won't hurt any one
+delay(60);
+
+ALCOHOL_detected = digitalRead(ALCOHOL_sensor);  
+      Serial.println(ALCOHOL_detected);  
+     if (ALCOHOL_detected == 1)  
+     {  
+      Serial.println("ALCOHOL detected...");  
+      tone(buzzPin,1000); 
+     }  
+     else  
+     {  
+      Serial.println("No ALCOHOL detected.");
+      noTone(buzzPin);
+//      digitalWrite(buzzPin, LOW);  
+     }    
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK converted!
+  p = finger.fingerSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID);
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+
+  return finger.fingerID;
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID);
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID;
+}
